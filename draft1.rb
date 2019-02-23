48b2136bb49bafa3c06e1ba1fcb817c04ff1ca0a
Kirsten Comandich <kcomandich@gmail.com>
2019-02-23 15:19:37 -0800

    Just trying things out to start with.

---
+++ draft1.rb
@@ -0,0 +1,10 @@
+#!/usr/bin/env ruby
+
+puts "Welcome to Zendesk Search"
+puts "Type 'quit' to exit at any time, Press 'Enter' to continue"
+puts "\n\n"
+puts "\tSelect search options:"
+puts "\t * Press 1 to search Zendesk"
+puts "\t * Press 2 to view a list of searchable fields"
+puts "\t * Type 'quit' to exit"
+puts "\n\n"
