1bc2135af97c766f21282a45f4db7c94f2d29c69
Kirsten Comandich <kcomandich@gmail.com>
2019-02-23 16:23:52 -0800

    Just starting to read in a JSON file.

--- draft1.rb
+++ draft1.rb
@@ -8,3 +8,9 @@ puts "\t * Press 1 to search Zendesk"
 puts "\t * Press 2 to view a list of searchable fields"
 puts "\t * Type 'quit' to exit"
 puts "\n\n"
+
+File.open('organizations.json','r') do |file|
+ file.readlines.each do |line|
+   puts line
+ end
+end
