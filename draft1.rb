a61fad6367b6d4d4f187e1c3abd0894d9bf4d401
Kirsten Comandich <kcomandich@gmail.com>
2019-02-25 16:02:25 -0800

    Add the User and Ticket structs.

--- draft1.rb
+++ draft1.rb
@@ -4,6 +4,8 @@ require 'readline'
 
 # TODO validate the JSON file against this structure
 Organization = Struct.new(:_id, :url, :external_id, :name, :domain_names, :created_at, :details, :shared_tickets, :tags)
+User = Struct.new(:_id, :url, :external_id, :name, :alias, :created_at, :active, :verified, :shared, :locale, :timezone, :last_login_at, :email, :phone, :signature, :organization_id, :tags, :suspended, :role)
+Ticket = Struct.new(:_id, :url, :external_id, :created_at, :type, :subject, :description, :priority, :status, :submitter_id, :assignee_id, :organization_id, :tags, :has_incidents, :due_at, :via)
 
 def intro_text
   puts "Welcome to Zendesk Search"
