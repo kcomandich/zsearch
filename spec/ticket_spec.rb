702d04ae920553b5a45966bb222a85d718d770e6
Kirsten Comandich <kcomandich@gmail.com>
2019-02-27 10:07:47 -0800

    Add Ticket specs.

---
+++ spec/ticket_spec.rb
@@ -0,0 +1,20 @@
+require 'ticket'
+
+RSpec.describe Ticket do
+
+  describe "#find" do
+    it 'returns a list of tickets that match an integer search criteria' do
+      korea = Ticket.new( 'subject' => "A Catastrophe in Korea (North)", 'submitter_id' => 38 )
+      micronesia = Ticket.new( 'subject' => "A Catastrophe in Micronesia", 'submitter_id' => 71 )
+      wallis = Ticket.new( 'subject' => "A Drama in Wallis and Futuna Islands", 'submitter_id' => 71 )
+      australia = Ticket.new( 'subject' => "A Drama in Australia", 'submitter_id' => 71 )
+      expect(Ticket.find('submitter_id', '71', [ korea, micronesia, wallis, australia ])).to eq([ micronesia, wallis, australia ])
+    end
+
+    it 'returns a list of tickets that match a string search criteria' do
+      korea = Ticket.new( 'subject' => 'A Catastrophe in Korea (North)' )
+      micronesia = Ticket.new( 'subject' => 'A Catastrophe in Micronesia' )
+      expect(Ticket.find('subject', 'A Catastrophe in Korea (North)', [ korea, micronesia ])).to eq([ korea ])
+    end
+  end
+end
