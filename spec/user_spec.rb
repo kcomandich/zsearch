f31bb0c66774488101f711b6e589a1a0ee4d8f78
Kirsten Comandich <kcomandich@gmail.com>
2019-02-27 09:56:55 -0800

    Move the 'search term not found' spec to the Record specs.

--- spec/user_spec.rb
+++ spec/user_spec.rb
@@ -4,10 +4,6 @@ require 'search'
 RSpec.describe User do
 
   describe '#find' do
-    it 'returns an error if the search term is not found' do
-      expect { User.find('fake', 1, []) }.to output(a_string_including("Search term not found")).to_stderr_from_any_process
-    end
-
     it 'returns a list of users that match an integer search criteria' do
       francesca = User.new( '_id' => 1 )
       cross = User.new( '_id' =>  2 )
